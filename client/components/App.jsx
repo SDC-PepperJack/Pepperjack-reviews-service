dc6aa58072ecce08941a9c8cf3674bb134aa2e36
james <jamesallstar21@gmail.com>
2019-10-10 15:37:19 -0400

    server and client is ready to go

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+
+
+  render() {
+    return (
+      <div>hello world! </div>
+    );
+  }
+}
+
+export default App;
