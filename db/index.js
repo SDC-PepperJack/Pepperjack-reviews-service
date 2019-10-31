bef79f7b2424ca9af5d42bf05c9324629c08adc2
Jonathan Yang <jonyang221@gmail.com>
2019-10-31 01:06:04 -0700

    Completed readme and finished CRUD operations.

--- db/index.js
+++ db/index.js
@@ -23,54 +23,49 @@ let postSellerData = (data, callback) => {
     ratings: data.ratings,
     comments: data.comments
   };
-
   Seller.collection.insertOne(newData, (err, results) => {
     if (err) {
       console.log(err);
     }
     callback(null, results);
   });
-
 };
 
 // gets sellers data
 let getSellerData = (sellerID, callback) => {
-
   Seller.findOne({ sellerID }, (err, results) => {
     if (err) {
       callback(err, null);
     }
     callback(null, results);
   });
-
 };
 
 // updates sellers data
 let putSellerData = (sellerID, values, callback) => {
-
   Seller.findByIdAndUpdate(sellerID, values, (err, results) => {
     if (err) {
       callback(err, null);
     }
     callback(null, results);
   });
-
 };
 
 // deletes sellers data
 let deleteSellerData = (sellerID, callback) => {
-
   Seller.findOneAndDelete({ sellerID }, (err, results) => {
     if (err) {
       callback(err, null);
     }
     callback(null, results);
   });
-
 };
 
 module.exports = {
   Seller,
-  getSellerData
+  getSellerData,
+  postSellerData,
+  putSellerData,
+  deleteSellerData
 };
 
